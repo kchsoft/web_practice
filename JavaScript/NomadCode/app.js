4fea896528dfafe9df830d38405b507c868b783c
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-08 15:07:51 +0900

    practice about toggle & transition
    using
    h1.classList.toggle( "anything" )
    +6 app.js
    
    & transition: color 0.5s ease-in-out
    +7 style.css
    
    toggle can add "any" if there is no "any" in classList <or> remove "any" if there is "any" in classList.
    
    & transition changes color slowly

--- JavaScript/NomadCode/app.js
+++ JavaScript/NomadCode/app.js
@@ -3,38 +3,7 @@ const h1 = document.querySelector('.hello h1');
 console.dir(h1);
 
 function handlerOnClick(){
-   h1.style.color="blue";
+   h1.classList.toggle("clicked");
 }
 
-function handlerMouseEnter(){
-   h1.innerHTML="Mouse is here";
-}
-
-function handlerMouseLeave(){
-   h1.innerHTML = "Mouse is gone!";
-}
-
-function handerlWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handlerWindowCopy(){
-   alert("copier!");
-}
-
-function handlerWindowOffline(){
-   alert("SOS no wifi");
-}
-
-function handlerWindowOnline(){
-   alert("ALL GOOOD!");
-}
-
-h1.addEventListener("click",handlerOnClick); // 1 way
-h1.addEventListener("mouseenter",handlerMouseEnter); // 2 way 이걸 더 선호
-h1.addEventListener("mouseleave",handlerMouseLeave); // removeEventListener가 가능하기 때문
-
-window.addEventListener("resize",handerlWindowResize);
-window.addEventListener("copy",handlerWindowCopy);
-window.addEventListener("offline",handlerWindowOffline);
-window.addEventListener("online",handlerWindowOnline);
+h1.addEventListener("click",handlerOnClick);
