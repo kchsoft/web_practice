196e47118ae1149097da3cc688b6cf94bef05f82
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-14 17:09:52 +0900

    practice about Font
    using
    font-size: px & rem +12,15 Font_size.html
    & <link> + 8 Web_Fonrs.html
    
    font-size에서 px와 rem의 차이, google font에서 font를 다운로드 받아 사용하는 방법에 대한 공부

--- JavaScript/NomadCode/App/JavaScript/weather.js
+++ JavaScript/NomadCode/App/JavaScript/weather.js
@@ -5,8 +5,9 @@ function onGeoOk(position){
     const lat = position.coords.latitude;    
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-
-   fetch(url)
+    // https://openweathermap.org/api
+   
+    fetch(url)
    .then((reponse) => reponse.json())
    .then((data)=>{
         const weather = document.querySelector("#weather span:first-child");
