02397004913c6820871c540435656e51e98917f3
changhyeon3 <53136613+changhyeon3@users.noreply.github.com>
2022-01-16 14:12:36 +0900

    practice about background-image
    using
    background-image: url(../) +4 background-image.css
    
    배경이미지 설정은 완료,
    랜덤으로 배경이미지를 설정하기 위해 background.js 파일에서 파일이름을 랜덤으로 받아 template을 이용해 class로 추가하면, background-image.css에서 적절한 class를 찾아 배경으로 설정한다는 시도를 해보았지만 실패함.

--- JavaScript/NomadCode/App/JavaScript/background.js
+++ JavaScript/NomadCode/App/JavaScript/background.js
@@ -1,6 +1,12 @@
-const images = ["0.jpg","1.jpg","2.jpg","3.jpg"];
-const chosenimage = images[Math.floor(Math.random()*images.length)];
+const images = ["0.jpg", "1.jpg"];
+const chosenimage = images[Math.floor(Math.random() * images.length)];
+document.body.classList.add(`background-${chosenimage}`);
 
-const backimage = document.createElement("img");
-backimage.src=`IMAGE/${chosenimage}`;
-document.body.appendChild(backimage);
+
+// 아래는 단순히 img 태그 삽입을 위한 코드
+
+// const backimage = document.createElement("img");
+// backimage.src=`IMAGE/${chosenimage}`;
+// // img 태그에서는 `TMAGE/` 를 하는 순간 자동으로 디렉토리를 한단계 벗어나서 IMAGE 디렉토리를 찾는 것 같음.
+
+// document.body.appendChild(backimage);
